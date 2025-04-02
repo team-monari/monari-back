f23f980ad23fcaa56797cefc6f85c6f46983c618
Joowon-Seo <tjwndnjs1998@gmail.com>
2025-04-02 16:28:09 +0900

    init

---
+++ src/main/java/com/monari/monariback/MonariBackApplication.java
@@ -0,0 +1,13 @@
+package com.monari.monariback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonariBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonariBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/monari/monariback/MonariBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monari.monariback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonariBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
