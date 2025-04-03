05063fae5fe15a76f698d815235aef88e9662127
HongJongMin <gwa7417@gmail.com>
2025-04-03 12:54:44 +0900

    feat: Entity 생성

---
+++ src/main/java/com/monari/monariback/lesson/entity/Lesson.java
@@ -0,0 +1,85 @@
+package com.monari.monariback.lesson.entity;
+
+import com.monari.monariback.lesson.entity.enurmurated.LessonStatus;
+import com.monari.monariback.lesson.entity.enurmurated.SchoolLevel;
+import com.monari.monariback.lesson.entity.enurmurated.Subject;
+import com.monari.monariback.location.entity.Location;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Getter
+@Table(name = "lesson")
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Lesson {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
+    private Location location;
+
+    @Column(name = "teacher_id", nullable = false)
+    private Integer teacherId;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "amount", nullable = false)
+    private Integer amount;
+
+    @Column(name = "min_student", nullable = false)
+    private Integer minStudent;
+
+    @Column(name = "max_student", nullable = false)
+    private Integer maxStudent;
+
+    @Column(name = "start_date", nullable = false)
+    private LocalDateTime startDate;
+
+    @Column(name = "end_date", nullable = false)
+    private LocalDateTime endDate;
+
+    @Column(name = "deadline", nullable = false)
+    private LocalDateTime deadline;
+
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private LessonStatus status;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @Column(name = "school_level")
+    @Enumerated(EnumType.STRING)
+    private SchoolLevel schoolLevel;
+
+    @Column(name = "subject")
+    @Enumerated(EnumType.STRING)
+    private Subject subject;
+
+}
