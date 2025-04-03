6a8ab12c3eefeae6d938629a2c759c847199f985
HongJongMin <gwa7417@gmail.com>
2025-04-03 12:54:56 +0900

    feat: Controller, Repo, Service 생성

---
+++ src/main/java/com/monari/monariback/lesson/controller/LessonController.java
@@ -0,0 +1,8 @@
+package com.monari.monariback.lesson.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LessonController {
+
+}
---
+++ src/main/java/com/monari/monariback/lesson/service/LessonService.java
@@ -0,0 +1,9 @@
+package com.monari.monariback.lesson.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LessonService {
+
+}
