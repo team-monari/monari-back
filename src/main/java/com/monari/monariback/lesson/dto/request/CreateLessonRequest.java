74945e8039292251bad299eb46e1c6fac77caec3
HongJongMin <gwa7417@gmail.com>
2025-04-04 10:46:36 +0900

    feat: 생성 수정 Dto

---
+++ src/main/java/com/monari/monariback/lesson/dto/request/CreateLessonRequest.java
@@ -0,0 +1,23 @@
+package com.monari.monariback.lesson.dto.request;
+
+import com.monari.monariback.lesson.entity.enurmurated.LessonStatus;
+import com.monari.monariback.lesson.entity.enurmurated.SchoolLevel;
+import com.monari.monariback.lesson.entity.enurmurated.Subject;
+import java.time.LocalDate;
+
+public record CreateLessonRequest(
+    Integer locationId,
+    Integer teacherId,
+    String description,
+    Integer amount,
+    Integer minStudent,
+    Integer maxStudent,
+    LocalDate startDate,
+    LocalDate endDate,
+    LocalDate deadline,
+    LessonStatus status,
+    SchoolLevel schoolLevel,
+    Subject subject
+) {
+
+}
