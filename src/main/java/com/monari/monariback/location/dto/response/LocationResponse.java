73b060ce075fb3999de95c6c5f31a1b65a335ba9
HongJongMin <gwa7417@gmail.com>
2025-04-07 15:37:24 +0900

    feat: LocationResponse 생성

---
+++ src/main/java/com/monari/monariback/location/dto/response/LocationResponse.java
@@ -0,0 +1,37 @@
+package com.monari.monariback.location.dto.response;
+
+import com.monari.monariback.location.entity.Location;
+import java.time.LocalDateTime;
+
+public record LocationResponse(
+    Integer id,
+    String serviceSubcategory,
+    String serviceStatus,
+    String paymentMethod,
+    String locationName,
+    String serviceUrl,
+    LocalDateTime registrationStartDateTime,
+    LocalDateTime registrationEndDateTime,
+    LocalDateTime cancellationStartDateTime,
+    LocalDateTime cancellationEndDateTime,
+    String cancellationPolicyInfo,
+    Integer cancellationDeadline
+) {
+
+    public static LocationResponse from(final Location location) {
+        return new LocationResponse(
+            location.getId(),
+            location.getServiceSubcategory(),
+            location.getServiceStatus(),
+            location.getPaymentMethod(),
+            location.getLocationName(),
+            location.getServiceUrl(),
+            location.getRegistrationStartDateTime(),
+            location.getRegistrationEndDateTime(),
+            location.getCancellationStartDateTime(),
+            location.getCancellationEndDateTime(),
+            location.getCancellationPolicyInfo(),
+            location.getCancellationDeadline()
+        );
+    }
+}
