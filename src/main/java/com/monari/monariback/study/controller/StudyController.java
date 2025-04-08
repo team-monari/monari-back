fe0874330176ed86e33c0cd7b0cce377a90fbd2b
Yunwoo Jeong <jeongmallro99@gmail.com>
2025-04-08 12:52:19 +0900

    feat: 스터디 조회 API 개발 (#21)
    - 스터디 상세 조회, 전체 조회 API 개발

--- src/main/java/com/monari/monariback/study/controller/StudyController.java
+++ src/main/java/com/monari/monariback/study/controller/StudyController.java
@@ -1,12 +1,14 @@
 package com.monari.monariback.study.controller;
 
+import com.monari.monariback.study.dto.request.StudyCreateRequest;
+import com.monari.monariback.study.dto.response.StudyResponse;
 import com.monari.monariback.study.service.StudyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.monari.monariback.study.dto.request.StudyCreateRequest;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,4 +22,17 @@ public class StudyController {
         studyService.createStudy(request);
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
+    @GetMapping()
+    public ResponseEntity<?> readStudies() {
+        List<StudyResponse> responses = studyService.readStudies();
+        return new ResponseEntity<>(responses, HttpStatus.OK);
+    }
+
+    @GetMapping("/{studyId}")
+    public ResponseEntity<StudyResponse> readStudy(@PathVariable("studyId") Integer studyId) {
+        StudyResponse response = studyService.readStudy(studyId);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
 }
