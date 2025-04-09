8ad3a3af616a1d5193ae6e868a47f2745e28d8ed
HongJongMin <gwa7417@gmail.com>
2025-04-09 16:55:04 +0900

    feat: QueryDsl 관련 어노테이션 추가

--- src/main/java/com/monari/monariback/global/config/WebConfig.java
+++ src/main/java/com/monari/monariback/global/config/WebConfig.java
@@ -1,23 +1,26 @@
 package com.monari.monariback.global.config;
 
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
+
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import lombok.RequiredArgsConstructor;
-
 @Configuration
 @RequiredArgsConstructor
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class WebConfig implements WebMvcConfigurer {
 
-	@Override
-	public void addCorsMappings(final CorsRegistry registry) {
-		registry.addMapping("/**")
-				.allowedOrigins("http://localhost:3000")
-				.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-				.allowCredentials(true)
-				.exposedHeaders(HttpHeaders.LOCATION);
-		WebMvcConfigurer.super.addCorsMappings(registry);
-	}
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+            .allowCredentials(true)
+            .exposedHeaders(HttpHeaders.LOCATION);
+        WebMvcConfigurer.super.addCorsMappings(registry);
+    }
 }
