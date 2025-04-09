1fb5559d83cb159c74b75c8747b60b70479a4e80
Joowon-Seo <tjwndnjs1998@gmail.com>
2025-04-09 16:20:53 +0900

    feat : auth userType  기반 접근 제어 AOP 도입 - @OnlyStudent, @OnlyTeacher 어노테이션 정의 및 처리 AOP(UserTypeAuthorizationAspect) 구현 - Accessor를 활용한 userType  검사 로직 추상화 - AuthException 처리 전용 핸들러 추가 (GlobalExceptionHandler)

--- src/main/java/com/monari/monariback/student/controller/StudentController.java
+++ src/main/java/com/monari/monariback/student/controller/StudentController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.monari.monariback.auth.aop.Auth;
+import com.monari.monariback.auth.aop.OnlyStudent;
 import com.monari.monariback.auth.entity.Accessor;
 import com.monari.monariback.student.dto.response.StudentResponse;
 import com.monari.monariback.student.service.StudentService;
@@ -19,6 +20,7 @@ public class StudentController {
 
 	private final StudentService studentService;
 
+	@OnlyStudent
 	@GetMapping("/me")
 	public ResponseEntity<StudentResponse> getMyProfile(@Auth Accessor accessor) {
 		return ResponseEntity.ok().body(
--- src/main/java/com/monari/monariback/teacher/controller/TeacherController.java
+++ src/main/java/com/monari/monariback/teacher/controller/TeacherController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.monari.monariback.auth.aop.Auth;
+import com.monari.monariback.auth.aop.OnlyTeacher;
 import com.monari.monariback.auth.entity.Accessor;
 import com.monari.monariback.teacher.dto.response.TeacherResponse;
 import com.monari.monariback.teacher.service.TeacherService;
@@ -19,6 +20,7 @@ public class TeacherController {
 
 	private final TeacherService teacherService;
 
+	@OnlyTeacher
 	@GetMapping("/me")
 	public ResponseEntity<TeacherResponse> getMyProfile(@Auth Accessor accessor) {
 		return ResponseEntity.ok().body(
