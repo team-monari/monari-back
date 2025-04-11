243ddac39089ed12406c9406df855e66034896e0
HongJongMin <gwa7417@gmail.com>
2025-04-11 18:22:46 +0900

    feat: 필터 및 선생이 개설한 수업 리스트업 구현

--- src/main/java/com/monari/monariback/lesson/repository/LessonCustomRepository.java
+++ src/main/java/com/monari/monariback/lesson/repository/LessonCustomRepository.java
@@ -1,5 +1,7 @@
 package com.monari.monariback.lesson.repository;
 
+import com.monari.monariback.common.enumerated.SchoolLevel;
+import com.monari.monariback.common.enumerated.Subject;
 import com.monari.monariback.lesson.entity.Lesson;
 import java.util.List;
 import org.springframework.stereotype.Repository;
@@ -9,10 +11,14 @@ public interface LessonCustomRepository {
 
     List<Lesson> findLessonsByPageSize(final Integer pageSize, final Integer pageNum);
 
-    List<Lesson> searchLessons(final String keyword, final Integer pageSize, final Integer pageNum);
+    List<Lesson> searchLessons(final String keyword, final Integer pageSize, final Integer pageNum,
+        final SchoolLevel schoolLevel, final Subject subject);
 
     int getTotalLessonPages(final int pageSize, final String keyword);
 
-    long getTotalLessonCount(final String keyword);
+    List<Lesson> findAllByTeacherId(final int teacherId, final int pageSize, final int pageNum);
+
+    long getTotalLessonCount(final String keyword, final SchoolLevel schoolLevel,
+        final Subject subject);
 
 }
