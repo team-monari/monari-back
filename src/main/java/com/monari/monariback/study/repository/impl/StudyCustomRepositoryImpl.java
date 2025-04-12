9e52315294bbab08984b5263a3b4ac006fd4562d
Yunwoo Jeong <jeongmallro99@gmail.com>
2025-04-12 17:20:05 +0900

    feat: 스터디 조회 API 개발 (#21) - fourth commit
    - 내가 개설한 스터디 조회 API 개발

--- src/main/java/com/monari/monariback/study/repository/impl/StudyCustomRepositoryImpl.java
+++ src/main/java/com/monari/monariback/study/repository/impl/StudyCustomRepositoryImpl.java
@@ -22,7 +22,7 @@ public class StudyCustomRepositoryImpl implements StudyCustomRepository {
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public List<StudyDto> findOrderByCreatedAtDesc(Integer pageNum, Integer pageSize) {
+    public List<StudyDto> findOrderByCreatedAtDesc(int pageNum, int pageSize) {
         return queryFactory
                 .select(Projections.constructor(StudyDto.class,
                         study.id,
@@ -47,7 +47,7 @@ public class StudyCustomRepositoryImpl implements StudyCustomRepository {
     }
 
     @Override
-    public List<StudyDto> findByKeywordOrderByCreatedAtDesc(Integer pageNum, Integer pageSize, String titleKeyword, String descriptionKeyword) {
+    public List<StudyDto> findByKeywordOrderByCreatedAtDesc(int pageNum, int pageSize, String titleKeyword, String descriptionKeyword) {
         return queryFactory
                 .select(Projections.constructor(StudyDto.class,
                         study.id,
@@ -67,6 +67,32 @@ public class StudyCustomRepositoryImpl implements StudyCustomRepository {
                 .innerJoin(study.student, student)
                 .where(containsTitleKeyword(titleKeyword))
                 .where(containsDescriptionKeyword(descriptionKeyword))
+                .orderBy(study.createdAt.desc(), study.id.desc())
+                .limit(pageSize)
+                .offset((long) pageSize * (pageNum - 1))
+                .fetch();
+    }
+
+    @Override
+    public List<StudyDto> findByStudentIdOrderByCreatedAtDesc(int pageNum, int pageSize, Integer studentId) {
+        return queryFactory
+                .select(Projections.constructor(StudyDto.class,
+                        study.id,
+                        study.title,
+                        study.description,
+                        study.subject,
+                        study.schoolLevel,
+                        study.status,
+                        study.createdAt,
+                        study.location.locationName,
+                        study.location.serviceUrl,
+                        study.student.publicId,
+                        study.student.name
+                        ))
+                .from(study)
+                .innerJoin(study.location, location)
+                .innerJoin(study.student, student)
+                .where(study.student.id.eq(studentId))
                 .orderBy(study.createdAt.desc(), study.id.desc())
                 .limit(pageSize)
                 .offset((long) pageSize * (pageNum - 1))
@@ -79,6 +105,16 @@ public class StudyCustomRepositoryImpl implements StudyCustomRepository {
                 .from(study)
                 .where(containsTitleKeyword(titleKeyword))
                 .where(containsDescriptionKeyword(descriptionKeyword))
+                .fetchOne();
+
+        return count != null ? count : 0L;
+    }
+
+    @Override
+    public long countByStudentId(Integer studentId) {
+        Long count = queryFactory.select(study.count())
+                .from(study)
+                .where(study.student.id.eq(studentId))
                 .fetchOne();
 
         return count != null ? count : 0L;
