5a6cceaf5473707a31dfc7cd9fce4500e43f12d2
HongJongMin <gwa7417@gmail.com>
2025-04-15 18:17:00 +0900

    feat: CORS 적용

--- src/main/java/com/monari/monariback/common/config/WebConfig.java
+++ src/main/java/com/monari/monariback/common/config/WebConfig.java
@@ -1,9 +1,10 @@
 package com.monari.monariback.common.config;
 
-import java.util.List;
-
 import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
+import com.monari.monariback.auth.aop.LoginArgumentResolver;
+import com.monari.monariback.auth.jwt.JwtInterceptor;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
@@ -13,38 +14,33 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.monari.monariback.auth.aop.LoginArgumentResolver;
-import com.monari.monariback.auth.jwt.JwtInterceptor;
-
-import lombok.RequiredArgsConstructor;
-
 @Configuration
 @RequiredArgsConstructor
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class WebConfig implements WebMvcConfigurer {
 
-	private final JwtInterceptor jwtInterceptor;
-	private final LoginArgumentResolver loginArgumentResolver;
+    private final JwtInterceptor jwtInterceptor;
+    private final LoginArgumentResolver loginArgumentResolver;
 
-	@Override
-	public void addCorsMappings(final CorsRegistry registry) {
-		registry.addMapping("/**")
-				.allowedOrigins("http://localhost:3000")
-				.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-				.allowCredentials(true)
-				.exposedHeaders(HttpHeaders.LOCATION);
-		WebMvcConfigurer.super.addCorsMappings(registry);
-	}
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("https://monari.site", "http://monari.site", "http://localhost:3000")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+            .allowCredentials(true)
+            .exposedHeaders(HttpHeaders.LOCATION);
+        WebMvcConfigurer.super.addCorsMappings(registry);
+    }
 
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(jwtInterceptor)
-				.addPathPatterns("/api/**")
-				.excludePathPatterns("/auth/**");
-	}
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtInterceptor)
+            .addPathPatterns("/api/**")
+            .excludePathPatterns("/auth/**");
+    }
 
-	@Override
-	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-		resolvers.add(loginArgumentResolver);
-	}
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginArgumentResolver);
+    }
 }
