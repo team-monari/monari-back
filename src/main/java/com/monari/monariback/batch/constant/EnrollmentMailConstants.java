a96c07ad9e0f06e8751efb1b07a59b97e63261ae
Yunwoo Jeong <jeongmallro99@gmail.com>
2025-05-09 21:40:34 +0900

    feat: 최소 수강 인원 미달로 인한 수업 취소 메일 전송
    - 역할에 따른 MessageBuilder 분리
    - MessageBuilder 분기문이 포함된 Factory 클래스 생성
    - 메일 전송 조건문 세분화

--- src/main/java/com/monari/monariback/batch/constant/EnrollmentMailConstants.java
+++
@@ -1,7 +0,0 @@
-package com.monari.monariback.batch.constant;
-
-public class EnrollmentMailConstants {
-    public static final String SERVICE_MAIL_ADDRESS = "monari@gmail.com";
-    public static final String LESSON_FEE_NOTICE_MAIL_SUBJECT = "[모나리] 수업 확정 및 수강료 안내드립니다.";
-    public static final String LESSON_FEE_NOTICE_MAIL_TEMPLATE = "lesson-fee-notice-mail.html";
-}
